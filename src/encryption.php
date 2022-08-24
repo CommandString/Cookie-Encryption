e255aab7bb5b086904090e5508659c304643face
root <root@CMDSTRPC>
2022-08-24 12:35:13 -0400

    v1.0.0

---
+++ src/encryption.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace cmdstr\cookieEncryption;
+
+use cmdstr\cookies\cookieEncryptionInterface;
+use cmdstr\encrypt\encryption as encryptor;
+
+class encryption implements cookieEncryptionInterface {
+    private encryptor $encryptor;
+
+    public function __construct(string $passphrase, string $encryptionMethod)
+    {
+        $this->encryptor = new encryptor($passphrase, $encryptionMethod);
+    }
+
+    public function encrypt(string|int $data):string
+    {
+        return $this->encryptor->encrypt($data);
+    }
+
+    public function decrypt(string $data):string
+    {
+        return $this->encryptor->decrypt($data);
+    }
+}
+
+?>
